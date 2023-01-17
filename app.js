56a96f182210f0d8fc7cd1dc506051d0a7656b2e
jihwan kim <jihwan@planz-coffee.com>
2023-01-17 23:05:15 +0900

    express server setting

---
+++ app.js
@@ -0,0 +1,21 @@
+const child = require("child_process");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const execSync = child.execSync("cat ~/.profile | grep IP", {
+  encoding: "utf-8",
+});
+const IP = execSync
+  .slice(execSync.indexOf("=") + 1)
+  .replace(/(\r\n|\n|\r)/gm, "")
+  .replace(/"/g, "");
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, IP || "localhost", () => {
+  console.log(IP);
+  console.log("서버가 실행됩니다.");
+});
